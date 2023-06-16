95998d90c7c7d76bc6ba92558a8d74f05e05e46f
Michel Dismer <m.dismer@velian.de>
2023-06-16 13:40:06 +0200

    allow peer dependencies

--- packages/dtslint/src/checks.ts
+++ packages/dtslint/src/checks.ts
@@ -38,6 +38,7 @@ export async function checkPackageJson(dirPath: string, typesVersions: readonly 
     switch (key) {
       case "private":
       case "dependencies":
+      case "peerDependencies":
       case "license":
       case "imports":
       case "exports":
